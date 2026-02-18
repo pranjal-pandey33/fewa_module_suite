89a35b327b20d24f88e068a244041a57d943350a
Fewa Technologies <fewatechnologies@gmail.com>
2026-02-18 19:37:00 +0545

    updates

--- fewa_module/modules/todo/lib/todo_module.dart
+++ fewa_module/modules/todo/lib/todo_module.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:foundation/foundation.dart';
-
+import 'dart:convert';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 import 'src/data/todo_projection_store.dart';
 
 class TodoModule {
   static const route = '/todo';
-  static final TodoProjectionStore projections = TodoProjectionStore();
+static int _calcCount = 0;
+static int get calcCount => _calcCount;
+static final TodoProjectionStore projections = TodoProjectionStore();
 
-  @override
-  Future<void> start(ModuleContext ctx) async {
-    await projections.init();
-    ctx.log("Projection store initialized.");
-  }
 
   static void register(RouteRegistry routes, EventBus bus, HookRegistry hooks) {
     routes.register(route, (_) => const TodoScreen());
@@ -21,38 +20,29 @@ class TodoModule {
       debugPrint('TodoModule contributed an action!');
     });
 
-    bus.subscribe<CalculationCompleted>((event) async {
-      await projections.init();
-      await projections.increment();
-      debugPrint("Todo received result: ${event.result} | total=${TodoModule.projections.calcCount.value}");
-    });
+bus.subscribe<CalculationCompleted>((event) async {
+  await projections.increment();
+});
+
 
   }
 }
 
 class TodoScreen extends StatefulWidget {
   const TodoScreen({super.key});
-
   @override
   State<TodoScreen> createState() => _TodoScreenState();
 }
 
 class _TodoScreenState extends State<TodoScreen> {
   @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
+    
     return  Scaffold(
       appBar: AppBar(title: Text('Todo')),
-      body: Center(
-        child: ValueListenableBuilder<int>(
-          valueListenable: TodoModule.projections.calcCount,
-          builder: (_, value, __) => Text("Calc events received: $value"),
-        ),
-      ),
+body: Center(
+  child: Text("Calc events received: ${TodoModule.calcCount}"),
+),
     );
   }
 }
